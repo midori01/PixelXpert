7d1ac993b02f60f2944792f5341220baa7544eb5
ミドリ <midori01@users.noreply.github.com>
2026-04-23 15:20:26 +0900

    Update UpdateFragment.java

--- app/src/main/java/sh/siava/pixelxpert/ui/fragments/UpdateFragment.java
+++ app/src/main/java/sh/siava/pixelxpert/ui/fragments/UpdateFragment.java
@@ -66,7 +66,7 @@ public class UpdateFragment extends BaseFragment {
 	public static final String MAGISK_MODULES_DIR = "/data/adb/modules";
 	private static final String updateRoot = String.format("%s/%s", MAGISK_UPDATE_DIR, MOD_NAME);
 
-	private static final String stableUpdatesURL = "https://raw.githubusercontent.com/midori01/PixelXpert/stable/latestStable.json";
+	private static final String stableUpdatesURL = "https://raw.githubusercontent.com/siavash79/PixelXpert/stable/latestStable.json";
 	private static final String canaryUpdatesURL = "https://raw.githubusercontent.com/midori01/PixelXpert/canary/latestCanary.json";
 	DownloadManager downloadManager;
 	long downloadID = 0; //from download manager
